4bede499f14bd5c23a9f7640e5451bbf89bd119b
cativ3 <63284801+cativ3@users.noreply.github.com>
2021-01-31 19:05:39 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,84 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICarService carManager = new CarManager(new InMemoryCarDal());
+
+            WriteTitle("GetAll");
+
+            WriteConsole(carManager);
+
+            WriteTitle("Add");
+
+            carManager.Add(new Car
+            {
+                Id = 6,
+                BrandId = 2,
+                ColorId = 3,
+                ModelYear = 2007,
+                DailyPrice = 800,
+                Description = "Kırmızı renk 2007 model BMW."
+            });
+
+            WriteConsole(carManager);
+
+            WriteTitle("Delete");
+
+            carManager.Delete(new Car
+            {
+                Id = 2,
+                BrandId = 3,
+                ColorId = 1,
+                ModelYear = 2008,
+                DailyPrice = 500,
+                Description = "Beyaz renk 2008 model Peugeot."
+            });
+
+            WriteConsole(carManager);
+
+            WriteTitle("GetById");
+
+            Console.WriteLine(carManager.GetById(3).Description);
+
+            WriteTitle("Update");
+
+            carManager.Update(new Car
+            {
+                Id = 3,
+                BrandId = 1,
+                ColorId = 4,
+                ModelYear = 2012,
+                DailyPrice = 900,
+                Description = "Lacivert renk 2012 model Mazda."
+            });
+
+            WriteConsole(carManager);
+            
+        }
+
+        // Konsol için yardımcı metotlar
+
+        static void WriteConsole(ICarService carManager)
+        {
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description);
+            }
+        }
+
+        static void WriteTitle(string methodName)
+        {
+            Console.WriteLine("\n-----------------------------");
+            Console.WriteLine($"{methodName} fonksiyonu:");
+            Console.WriteLine("-----------------------------");
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemoryCarDal.cs
@@ -0,0 +1,96 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>{
+                new Car
+                {
+                    Id = 1,
+                    BrandId = 1,
+                    ColorId = 3,
+                    ModelYear = 2006,
+                    DailyPrice = 400,
+                    Description = "Kırmızı renk 2006 model Mazda."
+                },
+                new Car
+                {
+                    Id = 2,
+                    BrandId = 3,
+                    ColorId = 1,
+                    ModelYear = 2008,
+                    DailyPrice = 500,
+                    Description = "Beyaz renk 2008 model Peugeot."
+                },
+                new Car
+                {
+                    Id = 3,
+                    BrandId = 2,
+                    ColorId = 2,
+                    ModelYear = 2016,
+                    DailyPrice = 1000,
+                    Description = "Siyah renk 2016 model BMW."
+                },
+                new Car
+                {
+                    Id = 4,
+                    BrandId = 2,
+                    ColorId = 5,
+                    ModelYear = 2020,
+                    DailyPrice = 1300,
+                    Description = "Gri renk 2020 model BMW."
+                },
+                new Car
+                {
+                    Id = 5,
+                    BrandId = 1,
+                    ColorId = 4,
+                    ModelYear = 2010,
+                    DailyPrice = 650,
+                    Description = "Lacivert renk 2010 model Mazda."
+                }
+            };
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public Car GetById(int id)
+        {
+            Car choosenCar = _cars.SingleOrDefault(c => c.Id == id);
+            return choosenCar;
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+        }
+    }
+}
