7a4c9a31b2b4cf87487aad8a0d7b179e9f2988a3
cativ3 <63284801+cativ3@users.noreply.github.com>
2021-02-16 12:09:03 +0300

    Update 16.02.2021

---
+++ Business/Concrete/RentalManager.cs
@@ -0,0 +1,42 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class RentalManager : IRentalService
+    {
+        IRentalDal _rentalDal;
+        public RentalManager(IRentalDal rentalDal)
+        {
+            _rentalDal = _rentalDal;
+        }
+        public IResult Add(Rental entity)
+        {
+            _rentalDal.Add(entity);
+            return new SuccessResult(Messages.Added);
+        }
+
+        public IResult Delete(Rental entity)
+        {
+            _rentalDal.Delete(entity);
+            return new SuccessResult(Messages.Deleted);
+        }
+
+        public IDataResult<List<Rental>> GetAll()
+        {
+            return new SuccessDataResult<List<Rental>>(Messages.Listed, _rentalDal.GetAll());
+        }
+
+        public IResult Update(Rental entity)
+        {
+            _rentalDal.Update(entity);
+            return new SuccessResult(Messages.Updated);
+        }
+    }
+}
